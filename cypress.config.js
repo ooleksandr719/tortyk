e137e3fceea90d4a5700a9d4e2dda50204069ff3
oleksandr719 <olry719@gmail.com>
2023-03-24 17:52:45 +0200

    second

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  projectId: "wowore",
+  projectId: "3wnvf5",
   e2e: {
+    baseUrl:'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
